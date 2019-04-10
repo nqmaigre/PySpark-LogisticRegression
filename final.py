5b05d491c08719f9662aab0e7477808e62bb9455
nq.maigre <nq.maigre@gmail.com>
2019-04-10 17:08:03 +0800

    update checkpoint

--- final.py
+++ final.py
@@ -8,7 +8,6 @@ import random
 import pyspark as sp
 from pyspark.conf import SparkConf
 from pyspark.context import SparkContext
-from datetime import datetime
 from pyspark.sql import SQLContext
 from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.feature import StandardScaler, Normalizer, ChiSqSelector
@@ -20,40 +19,62 @@ from pyspark.mllib.evaluation import BinaryClassificationMetrics, MulticlassMetr
 from pyspark.mllib.tree import RandomForest
 from pyspark.mllib.classification import SVMModel, SVMWithSGD
 
+# disable the warnings
 logging.getLogger('tensorflow').disabled = True
 
-# 读取文件
+# data needed has been created and saved in raw_data.h5 and gen_data.h5
+
+# read the raw data from raw_data.h5
+# the raw data is from teacher and the data has been processed by process_data.py
+# the raw data is of 138 days
 raw_h5f = h5py.File('raw_data.h5', 'r')
-datas = raw_h5f['datas'][:]
-others = raw_h5f['others'][:]
-labels = raw_h5f['labels'][:]
+datas = raw_h5f['datas'][:] # an array which has 138 elements
+# print(datas.shape) 
+# the shape of datas is (138, 32, 32, 144) 
+# 32*32 refers to the 32*32 grids
+# 144 = 48*3, 48 means that every day has 48 slots
+# 3 means that for every slot, inflow, outflow and inflow-outflow are taken into account
+others = raw_h5f['others'][:] # an array which has 138 elements
+# print(others.shape) 
+# the shape of others is (138, 67)
+# 67 refers to other information such as weather and windspeed 
+labels = raw_h5f['labels'][:] # an array which has 138 elements
+# print(labels.shape) 
+# the shape of labels is (138, 2)
+# [1, 0] means weekend and [0, 1] weekday
 raw_h5f.close()
 print('read raw data ok')
 
-# 读取文件
+# read the generated data from gen_data.h5 
+# the data was generated in the way of Data Augmentation
+# Data Augmentation helps to avoid overfitting
 gen_h5f = h5py.File('gen_data.h5', 'r')
 gen_d = gen_h5f['datas'][:]
+# print(gen_d.shape)
+# the shape of gen_d is (828, 32, 32, 144)
 gen_o = gen_h5f['others'][:]
+# print(gen_o.shape)
+# the shape of gen_d is (828, 67)
 gen_l = gen_h5f['labels'][:]
+# print(gen_l.shape)
+# the shape of gen_l is (828, 2)
+gen_h5f.close()
 print('read gen data ok')
 
-# 打乱顺序
-num_example = datas.shape[0]
-arr = np.arange(num_example)
-np.random.shuffle(arr)
-datas = datas[arr]
-labels = labels[arr]
-others = others[arr]
-
-# 将所有数据分为训练集和验证集
+# use raw data as test data
 x_test = datas
 y_test = labels
 o_test = others
 
+# use generated data as training data
 x_train = gen_d 
-o_train = gen_o 
 y_train = gen_l 
+o_train = gen_o 
 
+# following are some functions used in making the neural network
+# use CNN to extract the information from datas/gen_d (namely the information from inflows and outflows)
+# use normal NN to extract the information from others/gen_o
+# then combine the information extracted by CNN and the information extracted from others/gen_o, and use following neural network to do classification work
 def weight_variable(shape):
 	initial = tf.truncated_normal(shape, stddev=0.1)
 	return tf.Variable(initial)
@@ -73,12 +94,11 @@ def max_pool_4x4(x):
 	return tf.nn.max_pool(x, ksize=[1, 4, 4, 1],
                     	strides=[1, 4, 4, 1], padding='SAME')
 
-datas_placeholder = tf.placeholder(tf.float32, [None, 32, 32, 48*3])
-labels_placeholder = tf.placeholder(tf.float32, [None, 2])
+datas_placeholder = tf.placeholder(tf.float32, [None, 32, 32, 48*3]) # use datas/gen_d as input
+other_placeholder = tf.placeholder(tf.float32, [None, 67]) # use others/gen_o as input
+labels_placeholder = tf.placeholder(tf.float32, [None, 2]) # use labels/gen_l as input
 dropout_placeholder = tf.placeholder(tf.float32)
-other_placeholder = tf.placeholder(tf.float32, [None, 67])
 
-# best
 # num_conv1 = 16 # 第二层卷积核数量
 # num_conv2 = 32 # 第二层卷积核数量
 # num_fc = 128 # inflow outflow 特征提取数量
@@ -86,18 +106,20 @@ other_placeholder = tf.placeholder(tf.float32, [None, 67])
 # num_o_fc1 = 128 # 其他信息中提取出的特振数量
 # num_fc2 = 128 # 所有特征提取出的特征数量
 
-num_conv1 = 16 # 第二层卷积核数量
-num_conv2 = 32 # 第二层卷积核数量
-num_fc = 128 # inflow outflow 特征提取数量
-num_other = 67 # 其他信息的特征数量
-num_o_fc1 = 128 # 其他信息中提取出的特振数量
-num_fc2 = 64 # 所有特征提取出的特征数量
+num_conv1 = 16 # the amount of convolution kernels of layer1
+num_conv2 = 32 # the amount of convolution kernels of layer2
+num_fc = 128 # the amount of features extraced by CNN
+num_other = 67 # the dimensions of others/gen_o
+num_o_fc1 = 128 # the amount of features extraced by normal NN
+num_fc2 = 64 # the amount of features extraced by the total network
 
+# convulutional layer1
 W_conv1 = weight_variable([5, 5, 48*3, num_conv1])
 b_conv1 = bias_variable([num_conv1])
 h_conv1 = tf.nn.relu(conv2d(datas_placeholder, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
+# convulutional layer2
 W_conv2 = weight_variable([5, 5, num_conv1, num_conv2])
 b_conv2 = bias_variable([num_conv2])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
@@ -107,24 +129,26 @@ b_fc1 = bias_variable([num_fc])
 h_pool2_flat = tf.reshape(h_pool2, [-1, 8*8*num_conv2])
 W_fc1 = weight_variable([8*8*num_conv2, num_fc])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-h_fc1_drop = tf.nn.dropout(h_fc1, dropout_placeholder)
+h_fc1_drop = tf.nn.dropout(h_fc1, dropout_placeholder) # use dropout to avoid overfitting
 
 W_o_fc1 = weight_variable([num_other, num_o_fc1])
 b_o_fc1 = bias_variable([num_o_fc1])
 h_o_fc1 = tf.nn.relu(tf.matmul(other_placeholder, W_o_fc1) + b_o_fc1)
-h_o_fc1_drop = tf.nn.dropout(h_o_fc1, dropout_placeholder)
+h_o_fc1_drop = tf.nn.dropout(h_o_fc1, dropout_placeholder) # use dropout to avoid overfitting
 
+# concatenate the features extracted from CNN and normal NN
 h_concat = tf.concat([h_fc1_drop, h_o_fc1_drop], axis=1) #tf.reshape(tf.concat(1, [h_fc1, other_placeholder]), [-1, num_fc+67])
 
 W_fc2 = weight_variable([num_fc+num_o_fc1, num_fc2])
 b_fc2 = bias_variable([num_fc2])
 h_fc2 = tf.nn.relu(tf.matmul(h_concat, W_fc2) + b_fc2)
-h_fc2_drop = tf.nn.dropout(h_fc2, dropout_placeholder)
+h_fc2_drop = tf.nn.dropout(h_fc2, dropout_placeholder) # use dropout to avoid overfitting
 
 W_fc3 = weight_variable([num_fc2, 2])
 b_fc3 = bias_variable([2])
 y_conv = tf.nn.softmax(tf.matmul(h_fc2_drop, W_fc3) + b_fc3)
 
+# add L2 regularization to avoid overfitting
 tf.add_to_collection(tf.GraphKeys.WEIGHTS, W_conv1)
 tf.add_to_collection(tf.GraphKeys.WEIGHTS, W_conv2)
 tf.add_to_collection(tf.GraphKeys.WEIGHTS, W_fc1)
@@ -135,8 +159,7 @@ regularizer = tf.contrib.layers.l2_regularizer(scale=10.0/x_test.shape[0])
 reg_term = tf.contrib.layers.apply_regularization(regularizer)
 
 cross_entropy = -tf.reduce_sum(labels_placeholder*tf.log(y_conv))
-
-loss = cross_entropy + reg_term
+loss = cross_entropy + reg_term # loss function
 
 train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(labels_placeholder,1))
@@ -146,10 +169,15 @@ sess = tf.InteractiveSession()
 sess.run(tf.initialize_all_variables())
 
 saver = tf.train.Saver()
-saver = tf.train.import_meta_graph('./checkpoint/MyModel.meta')
-saver.restore(sess, tf.train.latest_checkpoint('./checkpoint'))
+# the model has been trained and saved, so now can be restored
+ckpt = tf.train.get_checkpoint_state('./checkpoint/')
+saver.restore(sess, ckpt.all_model_checkpoint_paths[0])
+print(ckpt)
 
-for i in range(500):
+# because the model has been trained and resotred, so it doesn't have to be trained now
+'''
+# train the model
+for i in range(10000):
 	rand_index = np.random.choice(x_train.shape[0], size = 32)
 	rand_x = x_train[rand_index]
 	rand_y = y_train[rand_index]
@@ -172,6 +200,9 @@ for i in range(500):
 		train_loss = cross_entropy.eval(train_feed_dict)
 		print("step %d, accuracy %g, test_accuracy %g, loss %g"%(i, train_accuracy, test_accuracy, train_loss))
 
+	# if (i+1) % 1000 == 0:
+	# 	saver.save(sess, './checkpoint/MyModel', global_step=(i+1))
+
 	train_step.run(feed_dict={
 		datas_placeholder: rand_x, 
 		labels_placeholder: rand_y, 
@@ -180,6 +211,9 @@ for i in range(500):
 	})
 
 print('train ends')
+'''
+
+# use raw data to test the model
 test_feed_dict = {
 	datas_placeholder: x_test, 
 	labels_placeholder: y_test, 
@@ -189,11 +223,18 @@ test_feed_dict = {
 test_accuracy = accuracy.eval(test_feed_dict)
 test_loss = cross_entropy.eval(test_feed_dict)
 
-# saver.save(sess, './checkpoint_dir/MyModel')
-
 print("accuracy on test data %g, loss on test data %g"%(test_accuracy, test_loss))
+# now the classification work finished, and got 100% accuracy
+# os._exit(0)
 
 
+# also, the features extracted by neural network can be saved
+# then use the extracted features to feed the spark model
+
+# following is using spark to do the classification work
+# the input is provided by neural network
+
+# extract the features of raw data
 raw_data = []
 for i in range(len(x_test)):
 	d = x_test[i]
@@ -213,34 +254,28 @@ sc = SparkContext(conf = conf)
 rdd = sc.parallelize(raw_data)
 
 feature_rdd = rdd.map(lambda line: line[:-1])
-scaler = StandardScaler(withStd = True, withMean = True).fit(feature_rdd) # 进行数值的标准化
-feature_rdd = scaler.transform(feature_rdd) # 特征 rdd
-label_rdd = rdd.map(lambda line: line[-1:]) # 标签 rdd
-
-# sqlContext = SQLContext(sc)
-# df = sqlContext.createDataFrame(feature_rdd)
-# df.show(5, False)
-# os._exit(0)
+scaler = StandardScaler(withStd = True, withMean = True).fit(feature_rdd) 
+feature_rdd = scaler.transform(feature_rdd) # feature rdd
+label_rdd = rdd.map(lambda line: line[-1:]) # label rdd
+# now the 
 
 def getAUC(feature_rdd, label_rdd, do_pca, pca_k, model_type, model_iterations):
-	# feature_rdd 特征值
-	# label_rdd 标签
-	# do_pca 是否进行主成分分析 True/False
-	# pca_k 主成分分析后保留的特征数
-	# model_type 使用的模型种类 1、线性回归 2、Logistic回归 3、Logistic回归 4、SVM 5、随即森林 （2、3都是逻辑回归）
-	# model_iterations 迭代次数，对于随机森林而言是决策树的数量
+	# feature_rdd 
+	# label_rdd 
+	# do_pca: whether do PCA to select features of not (True/False)
+	# pca_k: if do PCA, then how many features to select
+	# model_type: type of model (1. LinearRegressionWithSGD 
+	# 							 2. LogisticRegressionWithSGD 
+	#  							 3. LogisticRegressionWithLBFGS 
+	#					  		 4. SVM  5. RandomForest)
+	# model_iterations: the train iterations (for RandomForest, this parameter means the amount of trees)
 	pca_feature_rdd = feature_rdd
 	if do_pca:
 		pca_model = PCA(pca_k).fit(feature_rdd)
 		pca_feature_rdd = pca_model.transform(feature_rdd)
 
 	dataset = pca_feature_rdd.zip(label_rdd.map(lambda line: line[0])).map(lambda line: LabeledPoint(line[1], line[0]))
-	(train_data, test_data) = dataset.randomSplit([0.8, 0.2])
-
-	# sqlContext = SQLContext(sc)
-	# df = sqlContext.createDataFrame(dataset)
-	# df.show(5, False)
-	# os._exit(0)
+	(train_data, test_data) = dataset.randomSplit([0.8, 0.2]) # devide the dataset into train data and test data
 
 	model = None
 	predict = None
@@ -261,23 +296,23 @@ def getAUC(feature_rdd, label_rdd, do_pca, pca_k, model_type, model_iterations):
 		predict = model.predict(test_data.map(lambda p: p.features)).map(lambda p: float(p))
 
 	predict_real = predict.zip(test_data.map(lambda p: p.label)) 
-
 	metrics_bi = BinaryClassificationMetrics(predict_real)
 	metrics_mul = MulticlassMetrics(predict_real)
 
 	AUC = metrics_bi.areaUnderROC
-	# 一下三项好像不准确
 	precision = metrics_mul.precision()
 	recall = metrics_mul.recall()
 	f1Score = metrics_mul.fMeasure()
 
-	print(dataset.count())
-	print(predict_real.count()) # 打印 predict_real 的数据数量
-	print(metrics_mul.confusionMatrix()) # 打印混淆矩阵
+	print('dataset size: ', dataset.count())
+	print('test data size: ', predict_real.count())
+	print('confusion matrix:\n', metrics_mul.confusionMatrix()) # 打印混淆矩阵
 
 	return AUC, recall, precision, f1Score
 
 AUC, recall, precision, f1Score = getAUC(feature_rdd, label_rdd, False, -1, 2, 1000)
-print(AUC, recall, precision, f1Score)
-
+print('AUC: ', AUC)
+print('recall: ', recall)
+print('precision: ', precision)
+print('f1Score: ', f1Score)
 
---
+++ process_data.py
@@ -0,0 +1,195 @@
+import h5py
+import numpy as np
+from datetime import datetime
+import tensorflow as tf
+import logging
+import os
+import random
+
+logging.getLogger('tensorflow').disabled = True
+
+
+def isWeekend(date):
+	day = datetime.strptime(date, '%Y%m%d').weekday()
+	# Monday - Sunday
+	# 0      - 6
+	if day <= 4: 
+		return False
+	else:
+		return True
+
+f1 = h5py.File("BJ16_M32x32_T30_InOut.h5", "r")
+f2 = h5py.File("BJ_Meteorology.h5", "r")
+
+# Transform a datasets in a .h5 file into an numpy array.
+
+timeslots = f1["date"][:]
+# A array of shape 7220x1 where date[i] is the time of i-th timeslot.
+
+data = f1["data"][:]
+# An array of shape 7220x2x32x32 where the first dimension represents the index of timeslots. data[i][0] is a (32, 32) inflow matrix and data[i][1] is a (32, 32) outflow matrix.
+
+temperature = f2["Temperature"][:]
+# An array of shape 7220x1 where temperature[i] is the temperature at i-th timeslot.
+
+weather = f2["Weather"][:]
+# An array of shape 7220x17 where weather[i] is a one-hot vector which means:
+# [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] Sunny
+# [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] Cloudy
+# [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] Overcast
+# [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] Rainy
+# [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] Sprinkle
+# [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ModerateRain
+# [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] HeavyRain
+# [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0] Rainstorm
+# [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0] Thunderstorm
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0] FreezingRain
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0] Snowy
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0] LightSnow
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0] ModerateSnow
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0] HeavySnow
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0] Foggy
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0] Sandstorm
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1] Dusty
+
+windspeed = f2["WindSpeed"][:]
+# An array of shape 7220x1 where windspeed[i] is the wind speed at i-th timeslot.
+
+f1.close()
+f2.close()
+
+# select the days which have 48 slots
+# some days may just have 46 or 47 slots (these days should be deleted from data)
+d = dict()
+for i in range(7220):
+	timeslot = timeslots[i].decode('utf8')
+	date = timeslot[:8]
+	slot = int(timeslot[8:])
+
+	if date in d:
+		d[date][slot-1] = 1
+	else:
+		d[date] = 48*[0]
+		d[date][slot-1] = 1
+
+s = set()
+for key in d:
+	temp = d[key]
+	valid = True
+	for i in range(48):
+		if temp[i] == 0:
+			valid = False
+			break
+
+	if valid:
+		s.add(key)
+
+# use selected days to generate raw data
+datas = []
+labels = []
+others = []
+i = 0
+while True:
+	if(i >= 7220):
+		break
+
+	timeslot = timeslots[i].decode('utf8')
+	date = timeslot[:8]
+	if not date in s:
+		i += 1
+		continue
+	is_weekend = isWeekend(date)
+	slot = int(timeslot[8:])
+
+	flow = 32*[32*[48*3*[0]]]
+	count = 0
+	for m in range(48):
+		# i+m 
+		inflow = data[i+m][0] # 32*32
+		outflow = data[i+m][1] # 32*32	
+		for j in range(32):
+			for k in range(32):
+				flow[j][k][3*m] = inflow[j][k]/100.0
+				flow[j][k][3*m+1] = outflow[j][k]/100.0
+				flow[j][k][3*m+1] = (inflow[j][k]-outflow[j][k])/100.0
+
+	datas.append(flow)
+	labels.append([1, 0] if is_weekend else [0, 1])
+
+	item = []
+	item += [temperature[i].item()/10.0, windspeed[i].item()/10.0]
+	item += map(lambda x: int(x), weather[i].tolist())
+	slot_one_hot = [0] * 48
+	slot_one_hot[slot-1] = 1
+	item += slot_one_hot
+	others.append(item)
+
+	i += 48
+
+datas = np.array(datas)
+labels = np.array(labels)
+others = np.array(others)
+print(len(datas))
+print(len(labels))
+print(len(others))
+
+# raw data is saved in raw_data.h5
+raw_h5f = h5py.File('raw_data.h5', 'w')
+raw_h5f.create_dataset('datas', data=datas)
+raw_h5f.create_dataset('labels', data=labels)
+raw_h5f.create_dataset('others', data=others)
+raw_h5f.close()
+print('save raw data ok')
+
+
+# the function is to use raw data to generate more data
+def gen_new_data(raw_d, raw_o, raw_l, limit=10.0):
+	length = len(raw_l)
+	new_d = []
+	new_o = []
+	new_l = []
+	for i in range(length):
+		d = raw_d[i] # 32*32*(48*3)
+		o = raw_o[i] # 67
+		l = raw_l[i]
+		d_ = 32*[32*[48*3*[0]]]
+		o_ = 67*[0]
+		l_ = 2*[0]
+
+		for j in range(32):
+			for k in range(32):
+				for m in range(48*3):
+					d_[j][k][m] = d[j][k][m].copy()
+					d_[j][k][m] *= 1 + (random.uniform(-limit, limit)/100)
+					# print(d[j][k][m], ' ', d_[j][k][m])
+
+		for j in range(67):
+			o_[j] = o[j].copy()
+			o_[j] *= 1 + (random.uniform(-limit, limit)/100)
+			# print(o[j], ' ', o_[j])
+
+		l_ = l.copy()
+
+		new_d.append(d_)
+		new_o.append(o_)
+		new_l.append(l_)
+
+	return np.array(new_d), np.array(new_o), np.array(new_l)
+
+# use gen_new_data to generate more datas
+gen_d, gen_o, gen_l = gen_new_data(datas, others, labels, 30.0)
+for i in range(20):
+	print('gen batch %d'%(i+1))
+	new_d, new_o, new_l = gen_new_data(datas, others, labels, 15.0)
+	gen_d = np.concatenate((gen_d, new_d))
+	gen_o = np.concatenate((gen_o, new_o))
+	gen_l = np.concatenate((gen_l, new_l))
+
+# the generated data is saved in gen_data.h5
+gen_h5f = h5py.File('gen_data.h5', 'w')
+gen_h5f.create_dataset('datas', data=gen_d)
+gen_h5f.create_dataset('labels', data=gen_l)
+gen_h5f.create_dataset('others', data=gen_o)
+gen_h5f.close()
+print('save gen data ok')
+	
